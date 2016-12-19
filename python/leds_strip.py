03c34888163427e57276d7182ec628df92eaa239
Alejandro Gomez <alejodiscovery20@gmail.com>
2016-12-19 18:51:08 -0500

    Update and rename index.py to leds_strip.py

--- python/leds_strip.py
+++ python/leds_strip.py
@@ -1,17 +1,22 @@
 import time
 import serial
 
+ser = serial.Serial('/dev/cu.usbmodem1421', 9600)
+
 
 def send_value(led, R, G, B):
     string_led = str(led) + '-' + str(R) + '-' + str(G) + '-' + str(B) + '\n'
     ser.write(string_led)
+    time.sleep(0.02)
 
 
-ser = serial.Serial('/dev/cu.usbmodem1421', 9600)
+leds = 5
+step = 5
+delay = 0
 
 time.sleep(2)
 
-for i in range(0, 255, 5):
-    for j in range(0, 5):
+for i in range(0, 255, step):
+    for j in range(0, leds):
         send_value(j, i, 254 - i, 254 - i)
-    time.sleep(0.01)
+    time.sleep(delay)
